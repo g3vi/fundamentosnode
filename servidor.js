1f863e785f7954fd95a20f27359dde6c28c9a820
Gerardo Vieyra <g.vieyraaguirre@ugto.mx>
2024-02-12 13:28:10 -0600

    Instalacion de firebase

--- servidor.js
+++ servidor.js
@@ -1,6 +1,19 @@
 const express = require('express')
+import { initializeApp } from "firebase/app";
 const app = express()
 const port = 5002
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAI5H-3_qoxDHnEBY9DeO2AwKjOYVzdUQE",
+  authDomain: "crud-practica1-aedf1.firebaseapp.com",
+  projectId: "crud-practica1-aedf1",
+  storageBucket: "crud-practica1-aedf1.appspot.com",
+  messagingSenderId: "468172985668",
+  appId: "1:468172985668:web:9c5ad33fb46b9ef9e13311"
+};
+
+const app = initializeApp(firebaseConfig);
 
 app.get('/', (req, res) => {
     res.send('Respuesta Raiz 👏')
