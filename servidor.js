ab2952f54c8a4e0fe3ef5ef99363bf6d8f7f293e
Gerardo Vieyra <g.vieyraaguirre@ugto.mx>
2024-02-12 13:49:51 -0600

    Instalacion de dotenv, bcrypt y creacion de .env

--- servidor.js
+++ servidor.js
@@ -1,11 +1,12 @@
-const express = require('express')
+import express from 'express'
+import bcrypt from 'bcrypt'
+import 'dotenv/config'
 import { initializeApp } from "firebase/app";
-const app = express()
-const port = 5002
+import { getFirestore } from "firebase/app"
 
-
+// Conexion a la base de datos de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAI5H-3_qoxDHnEBY9DeO2AwKjOYVzdUQE",
+  apiKey: process.env.apiKey,
   authDomain: "crud-practica1-aedf1.firebaseapp.com",
   projectId: "crud-practica1-aedf1",
   storageBucket: "crud-practica1-aedf1.appspot.com",
@@ -13,7 +14,10 @@ const firebaseConfig = {
   appId: "1:468172985668:web:9c5ad33fb46b9ef9e13311"
 };
 
-const app = initializeApp(firebaseConfig);
+const firebase = initializeApp(firebaseConfig);
+const db = getFirestore()
+const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Respuesta Raiz 👏')
@@ -22,6 +26,8 @@ app.get('/', (req, res) => {
 app.get('/contacto', (req, res) => {
     res.send('Respuesta de contacto 👏')
 })
+
+const port = process.env.PORT || 6000
 
 app.listen(port, () => {
     console.log('Servidor en Funcionamiento 🐕: ',port)
